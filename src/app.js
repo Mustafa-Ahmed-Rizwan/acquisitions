544ebb337a8e118c02ea8471cd79e17612782c81
Mustafa <mustafahmed8110@gmail.com>
2025-10-05 20:03:56 +0500

    Added signup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
 // src/app.js
+import logger from '#config/logger.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import authRoutes from '#routes/auth.routes.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan('combined', { stream: { write: msg => logger.info(msg.trim()) } })
+);
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello From Acquisitions!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+app.get('/api',(req, res) => {
+  res.status(200).json({ message: 'Acquisitation Api is running!' });
+  
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
